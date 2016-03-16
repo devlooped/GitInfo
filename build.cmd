b61ea9ef6e0bf383eceba9052027203b7e0c5025
Daniel Cazzulino <daniel@cazzulino.com>
2016-03-16 18:06:05 -0300

    Update to latest v3.3 nuget

--- build.cmd
+++ build.cmd
@@ -10,7 +10,7 @@ SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
 IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe' -OutFile '%CACHED_NUGET%'"
 %LocalAppData%\NuGet\NuGet.exe Update -Self
 
 :copynuget
