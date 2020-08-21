e6dfa00b6dd292fe99c5ae64b1559e7ba171d105
James Clark <sbj@dimins.com>
2020-08-21 16:10:58 -0400

    use verbatim string literals in C# and F#
    to avoid escape sequence issues

--- src/GitInfo/build/GitInfo.fs.pp
+++ src/GitInfo/build/GitInfo.fs.pp
@@ -8,57 +8,57 @@ module Git =
     let [<Literal>] IsDirty = $GitIsDirty$
 
     /// <summary>IsDirtyString: $GitIsDirty$</summary>
-    let [<Literal>] IsDirtyString = "$GitIsDirty$"
+    let [<Literal>] IsDirtyString = @"$GitIsDirty$"
 
     /// <summary>Repository URL: $GitRepositoryUrl$</summary>
-    let [<Literal>] RepositoryUrl = "$GitRepositoryUrl$"
+    let [<Literal>] RepositoryUrl = @"$GitRepositoryUrl$"
 
     /// <summary>Branch: $GitBranch$</summary>
-    let [<Literal>] Branch = "$GitBranch$"
+    let [<Literal>] Branch = @"$GitBranch$"
 
     /// <summary>Commit: $GitCommit$</summary>
-    let [<Literal>] Commit = "$GitCommit$"
+    let [<Literal>] Commit = @"$GitCommit$"
 
     /// <summary>Sha: $GitSha$</summary>
-    let [<Literal>] Sha = "$GitSha$"
+    let [<Literal>] Sha = @"$GitSha$"
 
     /// <summary>Commits on top of base version: $GitCommits$</summary>
-    let [<Literal>] Commits = "$GitCommits$"
+    let [<Literal>] Commits = @"$GitCommits$"
 
     /// <summary>Tag: $GitTag$</summary>
-    let [<Literal>] Tag = "$GitTag$"
+    let [<Literal>] Tag = @"$GitTag$"
 
     /// <summary>Base tag: $GitBaseTag$</summary>
-    let [<Literal>] BaseTag = "$GitBaseTag$"
+    let [<Literal>] BaseTag = @"$GitBaseTag$"
 
     /// <summary>Provides access to the base version information used to determine the <see cref="SemVer" />.</summary>      
     module BaseVersion =
         /// <summary>Major: $GitBaseVersionMajor$</summary>
-        let [<Literal>] Major = "$GitBaseVersionMajor$"
+        let [<Literal>] Major = @"$GitBaseVersionMajor$"
 
         /// <summary>Minor: $GitBaseVersionMinor$</summary>
-        let [<Literal>] Minor = "$GitBaseVersionMinor$"
+        let [<Literal>] Minor = @"$GitBaseVersionMinor$"
 
         /// <summary>Patch: $GitBaseVersionPatch$</summary>
-        let [<Literal>] Patch = "$GitBaseVersionPatch$"
+        let [<Literal>] Patch = @"$GitBaseVersionPatch$"
 
 
     /// <summary>Provides access to SemVer information for the current assembly.</summary>
     module SemVer =
         /// <summary>Major: $GitSemVerMajor$</summary>
-        let [<Literal>] Major = "$GitSemVerMajor$"
+        let [<Literal>] Major = @"$GitSemVerMajor$"
 
         /// <summary>Minor: $GitSemVerMinor$</summary>
-        let [<Literal>] Minor = "$GitSemVerMinor$"
+        let [<Literal>] Minor = @"$GitSemVerMinor$"
 
         /// <summary>Patch: $GitSemVerPatch$</summary>
-        let [<Literal>] Patch = "$GitSemVerPatch$"
+        let [<Literal>] Patch = @"$GitSemVerPatch$"
 
         /// <summary>Label: $GitSemVerLabel$</summary>
-        let [<Literal>] Label = "$GitSemVerLabel$"
+        let [<Literal>] Label = @"$GitSemVerLabel$"
 
         /// <summary>Label with dash prefix: $GitSemVerDashLabel$</summary>
-        let [<Literal>] DashLabel = "$GitSemVerDashLabel$"
+        let [<Literal>] DashLabel = @"$GitSemVerDashLabel$"
 
         /// <summary>Source: $GitSemVerSource$</summary>
-        let [<Literal>] Source = "$GitSemVerSource$"
+        let [<Literal>] Source = @"$GitSemVerSource$"
