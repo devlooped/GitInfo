5be5956bd044a0f13c33856190a3fc3efa6c4475
Daniel Cazzulino <daniel@cazzulino.com>
2023-02-06 15:18:10 +0000

    Enable SponsorLink for improved sustainability
    This will allow me to be more responsive with support and ongoing maintenance.

---
+++ src/Analyzer/SponsorLinker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Devlooped;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace GitInfo;
+
+[Generator]
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic, LanguageNames.FSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base(SponsorLinkSettings.Create(
+        "devlooped", "GitInfo",
+        version: new Version(ThisAssembly.Info.Version).ToString(2)
+#if DEBUG
+        , quietDays: 0
+#endif
+        ))
+    { }
+}
