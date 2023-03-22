4e25c7c11038cf3217b4c0d68c6301c2f86d2582
Daniel Cazzulino <daniel@cazzulino.com>
2023-03-22 17:34:25 -0300

    Use three-segments version in telemetry

--- src/Analyzer/SponsorLinker.cs
+++ src/Analyzer/SponsorLinker.cs
@@ -11,7 +11,7 @@ class SponsorLinker : SponsorLink
     public SponsorLinker() : base(SponsorLinkSettings.Create(
         "devlooped", "GitInfo",
         packageId: "GitInfo",
-        version: new Version(ThisAssembly.Info.Version).ToString(2)
+        version: new Version(ThisAssembly.Info.Version).ToString(3)
 #if DEBUG
         , quietDays: 0
 #endif
